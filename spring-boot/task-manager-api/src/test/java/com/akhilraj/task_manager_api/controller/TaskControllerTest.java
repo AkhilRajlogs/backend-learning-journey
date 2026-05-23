4d1996cd858e3623afffdc38c4940531935ce715
Akhil Raj <akhilrajwork@gmail.com>
2026-05-23 21:56:05 +0530

    test: add MockMvc controller tests for task API

--- spring-boot/task-manager-api/src/test/java/com/akhilraj/task_manager_api/controller/TaskControllerTest.java
+++ spring-boot/task-manager-api/src/test/java/com/akhilraj/task_manager_api/controller/TaskControllerTest.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import com.akhilraj.task_manager_api.exception.TaskNotFoundException;
+import com.akhilraj.task_manager_api.model.Task;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 
@@ -27,17 +29,31 @@ public class TaskControllerTest {
     private MockMvc mockMvc;
 
     @MockBean
-    private TaskService taskService;
+    private TaskService taskService; 
+    // creates a mocked service layer to isolate controller testing
 
     @Test
     void shouldReturn200WhenGettingTasks() throws Exception {
+        // fails the test if checked exceptions occur during request execution
 
-        Page emptyPage = new PageImpl(List.of());
+        Page<Task> emptyPage = new PageImpl<>(List.of());
 
         when(taskService.getPaginatedTasks(0, 5, "id", "asc"))
                 .thenReturn(emptyPage);
+        // mock service returns an empty paginated response
 
         mockMvc.perform(get("/tasks"))
                 .andExpect(status().isOk());
+        // simulates GET /tasks request and expects HTTP 200 response
+    }
+
+    @Test
+    void shouldReturn404WhenTaskDoesNotExist() throws Exception {
+
+        when(taskService.getTaskById(999L))
+                .thenThrow(new TaskNotFoundException(999L));
+
+        mockMvc.perform(get("/tasks/999"))
+                .andExpect(status().isNotFound());
     }
 }
